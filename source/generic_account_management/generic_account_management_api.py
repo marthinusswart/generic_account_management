852ab270fc090fccd05c222a1532025ee64098f1
marthinusswart <matt.swart@icloud.com>
2022-09-26 17:34:30 +1000

    updated end points

--- source/generic_account_management/generic_account_management_api.py
+++ source/generic_account_management/generic_account_management_api.py
@@ -5,8 +5,8 @@ from generic_account_management.account_management import Accounts, AccountManag
 
 def create_api(app):
     api = Api(app)
-    api.add_resource(ApiManagement, '/')
-    api.add_resource(Accounts, '/<tenant_key>/accounts')
+    api.add_resource(ApiManagement, '/api/v1')
+    api.add_resource(Accounts, '/api/v1/<tenant_key>/accounts')
     api.add_resource(AccountManagement,
-                     '/<tenant_key>/accounts/<int:account_id>')
+                     '/api/v1/<tenant_key>/accounts/<int:account_id>')
     return api
